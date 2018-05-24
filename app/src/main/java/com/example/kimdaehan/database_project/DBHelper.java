68c5ec6d3fe0edbf7f2741a53e18133821fe04db
daehan <kimchdh1@naver.com>
2018-05-24 14:33:21 +0900

    test

---
+++ app/src/main/java/com/example/kimdaehan/database_project/DBHelper.java
@@ -0,0 +1,53 @@
+package com.example.kimdaehan.database_project;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("Create TABLE User (User_ID PRIMARY KEY, USER_Password) ;");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void insert(String create_at , String userId , String userPwd){
+        SQLiteDatabase db = getWritableDatabase() ;
+
+        db.execSQL("insert into user values("+userId+","+userPwd+")");
+
+
+        db.close();
+
+
+    }
+
+
+    public String getResult(){
+        SQLiteDatabase db = getReadableDatabase() ;
+        String result = "" ;
+
+        Cursor cursor = db.rawQuery("select * from user",null
+        ) ;
+
+        while (cursor.moveToNext()){
+
+            result +=cursor.getString(0)+":"
+                     +cursor.getString(1)+'\n' ;
+
+        }
+
+        return result ;
+    }
+}
+
